ecedbfc53bf78f154bf17e58da5958b9272b69b4
Rocket By <plplpl231212@gmail.com>
2021-10-29 00:47:08 +0300

    added fix in description of tests

--- src/test/java/tests/menu/NotLoggedUserTests.java
+++ src/test/java/tests/menu/NotLoggedUserTests.java
@@ -30,7 +30,7 @@ public class NotLoggedUserTests extends BaseTest {
     @Test
     @JiraIssue("HOMEWORK-257")
     @Tags({@Tag("web"), @Tag("decks")})
-    @DisplayName("Open page with decks by logged user")
+    @DisplayName("Open page with decks by not logged user")
     public void openDecksPageByNotLoggedUserTest(){
         new MainPage().clickMenuOption(MenuItem.OPEN_DECKS);
         new DecksPage()
@@ -42,7 +42,7 @@ public class NotLoggedUserTests extends BaseTest {
     @Test
     @JiraIssue("HOMEWORK-257")
     @Tags({@Tag("web"), @Tag("article")})
-    @DisplayName("Open page with articles by logged user")
+    @DisplayName("Open page with articles by not logged user")
     public void openArticlesPageByNotLoggedUserTest(){
         new MainPage().clickMenuOption(MenuItem.OPEN_ARTICLES);
         new ArticlesPage()
@@ -53,7 +53,7 @@ public class NotLoggedUserTests extends BaseTest {
     @Test
     @JiraIssue("HOMEWORK-257")
     @Tags({@Tag("web"), @Tag("article")})
-    @DisplayName("Open page to create a new article by logged user")
+    @DisplayName("Open page to create a new article by not logged user")
     public void openCreateArticlePageByNotLoggedUserTest() {
         mainPage.clickMenuOption(MenuItem.CREATE_ARTICLE);
         new LoginPage().loginPageShouldBeDisplayed();
@@ -62,7 +62,7 @@ public class NotLoggedUserTests extends BaseTest {
     @Test
     @JiraIssue("HOMEWORK-257")
     @Tags({@Tag("web"), @Tag("decks")})
-    @DisplayName("Open page to create a new deck by logged user")
+    @DisplayName("Open page to create a new deck by not logged user")
     public void openCreateDeckPageByNotLoggedUserTest() {
         mainPage.clickMenuOption(MenuItem.CREATE_DECK);
         new LoginPage().loginPageShouldBeDisplayed();
